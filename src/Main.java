591b3bc6d3bd2ac83ae9622984a89d6cc735892e
El_Tigere <eltigere@web.de>
2022-05-28 16:30:49 +0200

    funktioniert das?

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/*import engine.math.Vector;
+import engine.scene.Collisions;
+import engine.scene.GameObject;
+import engine.scene.Scene;
+import engine.scene.Collisions.LayerCollision;*/
+
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        /*
+        Scene s = new Scene(2, new LayerCollision[] {new Collisions.LayerCollision(0, 1, true)});
+        GameObject go1 = new TestGameObject();
+        GameObject go2 = new TestGameObject2();
+        go1.position = new Vector(100, 100);
+        go2.position = new Vector(350, 110);
+        s.addObject(go1);
+        s.addObject(go2);
+        s.startScene();
+        */
+        
+        System.out.println("Test");
+    }
+    
+}

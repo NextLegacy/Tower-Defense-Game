46c242a356681d22855ad6c1a50d2926a6fd21a0
NextLegacy <mcserezli@gmail.com>
2022-06-14 19:04:53 +0200

    Projectile Improvements

--- src/game/gameObjects/projectile/projectiles/HomingProjectile.java
+++ src/game/gameObjects/projectile/projectiles/HomingProjectile.java
@@ -4,33 +4,36 @@ import engine.math.Vector;
 import engine.scene.CollisionGameObject;
 import engine.utils.Sprite;
 import engine.utils.Lambda.Action2;
-import engine.utils.Lambda.Func0;
-import engine.utils.Lambda.Func1;
+import engine.utils.Lambda.Func2;
 import game.gameObjects.enemies.Enemy;
 import game.gameObjects.projectile.Projectile;
 
 public class HomingProjectile extends Projectile
 {
     private CollisionGameObject target;
-    private Func1<CollisionGameObject, Vector> getTarget;
+    private Func2<CollisionGameObject, Vector, Double> getTarget;
 
     private double speed;
-    Vector direction;
+    private Vector direction;
+    private double range;
 
-    public HomingProjectile(Sprite sprite, Vector position, CollisionGameObject target, double speed, Action2<Projectile, Enemy> onHit)
+    public HomingProjectile(Sprite sprite, double range, Vector position, CollisionGameObject target, double speed, Action2<Projectile, Enemy> onHit)
     {
         super(sprite, position, onHit);
         this.target = target;
         this.speed = speed;
-        this.direction = Vector.one();
+        this.range = range;
+        this.direction = position.directionTo(target.position);
     }
 
-    public HomingProjectile(Sprite sprite, Vector position, Func1<CollisionGameObject, Vector> getTarget, double speed, Action2<Projectile, Enemy> onHit)
+    public HomingProjectile(Sprite sprite, double range, Vector position, Vector startDirection, Func2<CollisionGameObject, Vector, Double> getTarget, double speed, Action2<Projectile, Enemy> onHit)
     {
         super(sprite, position, onHit);
         this.getTarget = getTarget;
+        this.range = range;
         this.speed = speed;
-        this.direction = Vector.zero();
+
+        this.direction = startDirection;
     }
 
 
@@ -38,7 +41,7 @@ public class HomingProjectile extends Projectile
     public void update(double deltaTime) 
     {
         if (getTarget != null)
-            target = getTarget.invoke(position);
+            target = getTarget.invoke(position, range);
 
         if (target != null && target.isActive())
             direction = target.position.sub(position).normalized();
